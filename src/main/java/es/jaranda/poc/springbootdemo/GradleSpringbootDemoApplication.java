d6fe7d9645edfc855657215a598f26bb984e9672
Jorge Aranda de Miguel <jorgearanda123@gmail.com>
2017-01-12 23:30:39 +0100

    Implementing dummy MainController

--- src/main/java/es/jaranda/poc/springbootdemo/GradleSpringbootDemoApplication.java
+++ src/main/java/es/jaranda/poc/springbootdemo/GradleSpringbootDemoApplication.java
@@ -3,6 +3,12 @@ package es.jaranda.poc.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @SpringBootApplication
 public class GradleSpringbootDemoApplication {
@@ -10,4 +16,39 @@ public class GradleSpringbootDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GradleSpringbootDemoApplication.class, args);
 	}
+
+
+
+}
+
+@XmlRootElement(name = "response")
+class ResponseDto {
+
+	private boolean success;
+
+    public ResponseDto() {
+
+    }
+
+    public ResponseDto(boolean success) {
+        this.success = success;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+}
+
+@RestController
+class MainController {
+
+    @RequestMapping(value = "main", method = GET)
+	public ResponseDto root() {
+	    return new ResponseDto(true);
+    }
+
 }
