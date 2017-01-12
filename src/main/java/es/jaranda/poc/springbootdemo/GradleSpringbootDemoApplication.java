97bd073719785f9937dc13c79e81c8ab846ca935
Jorge Aranda de Miguel <jorgearanda123@gmail.com>
2017-01-12 23:13:18 +0100

    Initial commit

---
+++ src/main/java/es/jaranda/poc/springbootdemo/GradleSpringbootDemoApplication.java
@@ -0,0 +1,13 @@
+
+package es.jaranda.poc.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleSpringbootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleSpringbootDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/es/jaranda/poc/springbootdemo/GradleSpringbootDemoApplicationTests.java
@@ -0,0 +1,18 @@
+
+package es.jaranda.poc.springbootdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GradleSpringbootDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+
+	}
+
+}
